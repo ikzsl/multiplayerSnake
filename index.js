8f266dfdd27582ba205f18e9885b3c57cc52d1d5
Muslim Djalaludinov <ikzsl@yandex.ru>
2021-04-25 16:05:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const BG_COLOR = '#231f20';
 const SNAKE_COLOR = '#c2c2c2';
 const FOOD_COLOR = '#e66916';
 
-const socket = io('https://194.87.144.199:80');
+const socket = io('http://194.87.144.199:80');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
